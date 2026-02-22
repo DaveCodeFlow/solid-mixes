4d75a61a1d9dc3aa61624830502af2268fa5baea
davecodeflow <davekorver@gmail.com>
2026-02-22 13:17:41 +0100

    solidMix3Live-sorting function

--- jsMain.js
+++ jsMain.js
@@ -3,15 +3,21 @@ console.log("JS loaded");
 const mixes = [
   {
     title: "SOLID – Episode 01",
-    date: "<span class='mix-date'>Jan 2026</span>",
+    date: "2026-01-01",
     mixcloudUrl:
       "https://www.mixcloud.com/DJ_Dave_K/solid-steady-beats-strong-flow-episode-01/",
   },
   {
     title: "SOLID – Episode 02",
-    date: "<span class='mix-date'>Feb 2026</span>",
-    mixcloudUrl: 
-    "https://www.mixcloud.com/DJ_Dave_K/solid-steady-beats-strong-flow-episode-02/"
+    date: "2026-02-10",
+    mixcloudUrl:
+      "https://www.mixcloud.com/DJ_Dave_K/solid-steady-beats-strong-flow-episode-02/",
+  },
+  {
+    title: "SOLID – Episode 03",
+    date: "2026-02-21",
+    mixcloudUrl:
+      "https://www.mixcloud.com/DJ_Dave_K/solid-steady-beats-strong-flow-episode-03/",
   },
 ];
 
@@ -28,24 +34,27 @@ toggleButton.addEventListener("click", () => {
   }
 });
 
-mixes.forEach((mix, index) => {
+const sortedMixes = [...mixes].sort(
+  (a, b) => new Date(b.date) - new Date(a.date),
+);
+sortedMixes.forEach((mix, index) => {
   const section = document.createElement("section");
   section.className = "mix";
 
-  if (index === 1) {
+  if (index === 0) {
     section.classList.add("featured");
   }
 
   section.innerHTML = `
-      <h2>${mix.title}</h2>
-      <span>${mix.date}</span>
-      <iframe 
-        src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&light=0&feed=${encodeURIComponent(mix.mixcloudUrl)}"
-        width="100%"
-        height="120"
-        frameborder="0">
-      </iframe>
-    `;
+  <h2>${mix.title}</h2>
+  <span class="mix-date">${mix.date}</span>
+  <iframe 
+    src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&light=0&feed=${encodeURIComponent(mix.mixcloudUrl)}"
+    width="100%"
+    height="120"
+    frameborder="0">
+  </iframe>
+`;
 
   container.appendChild(section);
 });
